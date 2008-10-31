cd6c03e582a03bf7231801506b8d1064d3051665
Corey Donohoe <atmos@corey-donohoes-macbook-pro.local>
2008-10-31 01:15:49 -0600

    updates so this can run under ci

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,23 +1,18 @@
-# dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "0.9.10"
-dm_gems_version   = "0.9.6"
-
-# For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
-dependency "merb-action-args", merb_gems_version
-dependency "merb-assets", merb_gems_version  
-dependency "merb-cache", merb_gems_version   
-dependency "merb-helpers", merb_gems_version 
-dependency "merb-mailer", merb_gems_version  
-dependency "merb-slices", merb_gems_version  
-dependency "merb-auth-core", merb_gems_version
-dependency "merb-auth-more", merb_gems_version
-dependency "merb-auth-slice-password", merb_gems_version
-dependency "merb-param-protection", merb_gems_version
-dependency "merb-exceptions", merb_gems_version
- 
-dependency "dm-core", dm_gems_version         
-dependency "dm-aggregates", dm_gems_version   
-dependency "dm-migrations", dm_gems_version   
-dependency "dm-timestamps", dm_gems_version   
-dependency "dm-types", dm_gems_version        
-dependency "dm-validations", dm_gems_version  
+# auto generated from config/gems.yml
+dependency 'extlib', '0.9.8'
+dependency 'merb-core', '0.9.12'
+dependency 'merb-param-protection', '0.9.12'
+dependency 'merb-haml', '0.9.12'
+dependency 'merb-auth-core', '0.9.12'
+dependency 'merb-auth-more', '0.9.12'
+dependency 'merb-cache', '0.9.12'
+dependency 'merb-helpers', '0.9.12'
+dependency 'merb-assets', '0.9.12'
+dependency 'merb-slices', '0.9.12'
+dependency 'merb-action-args', '0.9.12'
+dependency 'merb_datamapper', '0.9.6'
+dependency 'dm-core', '0.9.6'
+dependency 'dm-validations', '0.9.6'
+dependency 'dm-serializer', '0.9.6'
+dependency 'dm-timestamps', '0.9.6'
+dependency 'dm-types', '0.9.6'
