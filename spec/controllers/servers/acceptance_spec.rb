952e33aa7ddc2130eaa49cc8aa4c6be526adaa03
Corey Donohoe <atmos@atmos.org>
2008-11-04 14:47:48 -0700

    update to rc5 to see how things are, you need nokogiri or the has_xpath stuff is busted

--- spec/controllers/servers/acceptance_spec.rb
+++ spec/controllers/servers/acceptance_spec.rb
@@ -30,57 +30,7 @@ describe Servers, "#acceptance" do
       @response.should be_successful
     end
     it "should redirect to the login page" do
-      @response.should have_xpath("//form[@action='/servers/decision']")
+      @response.should have_xpath("//form[@action='/servers/decision' and @method='post']")
     end
   end
-  # describe "empty params" do
-  #   it "should raise errors" do
-  #     lambda { dispatch_to(Servers, :decision) }.should raise_error
-  #   end
-  # end
-  # 
-  # describe "decision screens" do
-  #   before(:each) do
-  #     @params = {"openid.mode"=>"checkid_setup", "openid.return_to" => 'http://localhost',
-  #               'openid.identity' => 'http://localhost/users/atmos'}
-  #     @check_id_request = OpenID::Server::CheckIDRequest.new('http://localhost/users/atmos', 
-  #                                                             'http://localhost', 
-  #                                                             @server.op_endpoint, 
-  #                                                             'http://localhost')
-  #     @message = OpenID::Message.new('http://specs.openid.net/auth/2.0')        
-  #     @check_id_request.message = @message
-  # 
-  #     @check_id_response = OpenID::Server::OpenIDResponse.new(@check_id_request)
-  #   end
-  # 
-  #   describe "cancel" do
-  #     before(:each) do
-  #       @response = dispatch_to(Servers, :decision, {:cancel => :cancel}) do |controller|
-  #         mock(controller.session).[](:last_oidreq) { @check_id_request }
-  #       end
-  #     end
-  #     it "should redirect the user" do
-  #       @response.status.should == 302
-  #     end
-  #   end
-  # 
-  #   describe "agreement" do
-  #     before(:each) do
-  #       mock(OpenID::Store::Filesystem).new(Merb.root / 'config' / 'openid-store') { @store }
-  #       mock(OpenID::Server::Server).new(@store, 'http://localhost/servers') { @server }
-  #       
-  #       mock(@check_id_request).answer(true, nil, 'http://localhost/users/atmos') { @check_id_response }
-  #       
-  #       @response = dispatch_to(Servers, :decision, {:yes => :yes}) do |controller|
-  #         mock(controller.session).[](:last_oidreq) { @check_id_request }
-  #       end
-  #     end
-  #     it "should be successful" do
-  #       @response.status.should == 302
-  #     end
-  #     it "should render the good stuff" do
-  #       @response.body.should match(%r!<a href="http://localhost\?.*?"!)
-  #     end
-  #   end
-  # end
 end
