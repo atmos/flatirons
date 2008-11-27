a2176a974f3da1a7ed273ce5f07c9d8b07e77394
Corey Donohoe <atmos@atmos.org>
2008-11-27 10:15:37 -0700

    add error_messages for on the unauthenticated page, add a spec to show failed to login messages in the markup, slight tweak of provider xrds specdoc wording

--- spec/requests/landing_page_spec.rb
+++ spec/requests/landing_page_spec.rb
@@ -4,6 +4,14 @@ describe "User Landing Page" do
       response = request("/")
       response.should be_a_valid_merb_auth_form
     end
+    it "should display form errors if they enter a bad username/password" do
+      response = request("/")
+      response.should be_a_valid_merb_auth_form
+      response = request "/login", :method => "PUT", 
+                                   :params => { :login => 'quentin', :password => 'foobarbaz' }
+      response.should be_a_valid_merb_auth_form
+      response.should have_selector("div.content div.error h2:contains('Unable to login')")
+    end
   end
   describe "requesting / when authenticated", :given => 'an authenticated user' do
     it "should display the landing page" do
--- spec/requests/openid/provider_xrds_spec.rb
+++ spec/requests/openid/provider_xrds_spec.rb
@@ -1,6 +1,6 @@
 describe "Provider URL Information" do
   describe "accepting xrds+xml" do
-    it "renders the provider idp page" do
+    it "renders the identity provider's xrds page (/servers/xrds)" do
       response = request("/servers/xrds", {'HTTP_ACCEPT' => 'application/xrds+xml'})
       response.should be_successful
       response.should have_xpath("//xrd/service[uri='http://example.org/servers']")
