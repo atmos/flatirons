75dd480925bf90d92b6ef614fc94f005d827b7be
Corey Donohoe <atmos@atmos.org>
2008-11-26 22:47:56 -0700

    remove traces of response.body when the have_xpath stuff runs just fine against the response object

--- spec/requests/openid/identity_discovery_spec.rb
+++ spec/requests/openid/identity_discovery_spec.rb
@@ -5,9 +5,9 @@ describe "Identity Discovery" do
       response = request("/users/quentin", {'HTTP_ACCEPT' => 'application/xrds+xml'})
       response.should be_successful
       response.headers["X-XRDS-Location"].should == "http://example.org/users/quentin/xrds"
-      response.body.should have_xpath("//xrd/service[uri='http://example.org/servers']")
-      response.body.should have_xpath("//xrd/service[type='http://specs.openid.net/auth/2.0/signon']")
-      response.body.should have_xpath("//xrd/service[type='http://openid.net/sreg/1.0']")
+      response.should have_xpath("//xrd/service[uri='http://example.org/servers']")
+      response.should have_xpath("//xrd/service[type='http://specs.openid.net/auth/2.0/signon']")
+      response.should have_xpath("//xrd/service[type='http://openid.net/sreg/1.0']")
     end
 
     it "gracefully handles non-existent user requests(/users/romeo)" do
@@ -21,9 +21,9 @@ describe "Identity Discovery" do
     it "renders the user's identity page" do
       response = request("/users/quentin")
       response.should be_successful
-      response.body.should have_xpath("//link[@rel='openid.server' and @href='http://example.org/servers']")
-      response.body.should have_xpath("//meta[@http-equiv='X-XRDS-Location' and @content='http://example.org/users/quentin/xrds']")
-      response.body.should have_xpath("//body[p='OpenID identity page for quentin']")
+      response.should have_xpath("//link[@rel='openid.server' and @href='http://example.org/servers']")
+      response.should have_xpath("//meta[@http-equiv='X-XRDS-Location' and @content='http://example.org/users/quentin/xrds']")
+      response.should have_xpath("//body[p='OpenID identity page for quentin']")
       response.headers["X-XRDS-Location"].should == "http://example.org/users/quentin/xrds"
     end
   end
--- spec/requests/openid/provider_xrds_spec.rb
+++ spec/requests/openid/provider_xrds_spec.rb
@@ -3,8 +3,8 @@ describe "Provider URL Information" do
     it "renders the provider idp page" do
       response = request("/servers/xrds", {'HTTP_ACCEPT' => 'application/xrds+xml'})
       response.should be_successful
-      response.body.should have_xpath("//xrd/service[uri='http://example.org/servers']")
-      response.body.should have_xpath("//xrd/service[type='http://specs.openid.net/auth/2.0/server']")
+      response.should have_xpath("//xrd/service[uri='http://example.org/servers']")
+      response.should have_xpath("//xrd/service[type='http://specs.openid.net/auth/2.0/server']")
     end
   end
   
