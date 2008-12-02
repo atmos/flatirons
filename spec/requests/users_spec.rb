f4a990142fb69f5a5c911fab0c177b4573723095
Corey Donohoe <atmos@atmos.org>
2008-12-01 21:40:44 -0700

    reorganizing this a tad

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,3 @@
-describe "Deleting Quentin's account" do
-  before(:each) { setup_user }
-  it "delete quentin's account successfully" do
-    response = request("/users/quentin", :method => 'delete')
-    response.should redirect_to(url(:new_user))
-  end
-end
 describe "Signup up for an account" do
   it "display a form requesting your email address" do
     response = request("/users/new")
@@ -12,5 +5,13 @@ describe "Signup up for an account" do
     response.should have_selector("form[action='/users'][method='post']")
     response.should have_selector("input#login[type='text'][name='login']")
     response.should have_selector("input#email[type='text'][name='email']")
+  end
+end
+
+describe "Deleting Quentin's account" do
+  before(:each) { setup_user }
+  it "delete quentin's account successfully" do
+    response = request("/users/quentin", :method => 'delete')
+    response.should redirect_to(url(:new_user))
   end
 end
