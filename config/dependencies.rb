ca1326490cac6be7e38c04443f4dc6ec8663853f
Corey Donohoe <atmos@atmos.org>
2009-02-17 10:53:59 -0700

    fixup the dependencies and update to the latest thor tasks

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,8 +1,12 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.6"
-dm_gems_version   = "0.9.8"
+merb_gems_version = "~>1.0.9"
+dm_gems_version   = "~>0.9.10"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
+dependency "dm-core", dm_gems_version         
+dependency "dm-validations", dm_gems_version  
+
+dependency "merb-core", merb_gems_version
 dependency "merb-gen", merb_gems_version
 dependency "merb-action-args", merb_gems_version
 dependency "merb-assets", merb_gems_version  
@@ -16,18 +20,14 @@ dependency "merb-auth-more", merb_gems_version
 dependency "merb-auth-slice-password", merb_gems_version
 dependency "merb-param-protection", merb_gems_version
 dependency "merb-exceptions", merb_gems_version
-
-dependency "merb-exceptions", merb_gems_version
-
-dependency "dm-core", dm_gems_version         
-dependency "dm-validations", dm_gems_version  
-dependency 'merb_datamapper', dm_gems_version
-
-dependency 'nokogiri', '>=1.0.6'
-dependency 'webrat', '=0.3.2'
-dependency 'rr'
-dependency 'rcov'
-dependency 'mongrel'
-dependency 'ruby-debug', '=0.10.3'
+dependency 'merb_datamapper', merb_gems_version
 
 dependency "ruby-openid", '=2.1.2', :require_as => 'openid'
+
+dependency 'nokogiri', '>=1.0.6',   :require_as => nil
+dependency 'webrat', '=0.3.2',      :require_as => nil
+dependency 'rr',                    :require_as => nil
+dependency 'rcov',                  :require_as => nil
+dependency 'mongrel',               :require_as => nil
+dependency 'ruby-debug', '=0.10.3', :require_as => nil
+
